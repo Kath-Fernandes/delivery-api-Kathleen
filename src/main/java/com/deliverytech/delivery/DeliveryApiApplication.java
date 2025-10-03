a3a6899a53b1e2a6a7d4d19983946e1a4977d84b
Kathleen Fernandes <kathleen.fernandes92@gmail.com>
2025-10-03 20:24:37 -0300

    :ok_hand: Alterações de revisão de código

--- src/main/java/com/deliverytech/delivery/DeliveryApiApplication.java
+++ src/main/java/com/deliverytech/delivery/DeliveryApiApplication.java
@@ -3,6 +3,16 @@ package com.deliverytech.delivery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
+@OpenAPIDefinition(
+    info = @Info(
+    title = "API de Clientes",
+    version = "1.0",
+    description = "Documentação da API para gerenciamento de clientes"
+)
+)
+
 @SpringBootApplication
 public class DeliveryApiApplication {
 
